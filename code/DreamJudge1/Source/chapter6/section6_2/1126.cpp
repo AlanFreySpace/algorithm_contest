bce45d09a297b31f9b6761f3f30b6e5cb0c91202
AlanFreySpace <976167955@qq.com>
2022-02-08 19:46:20 +0800

    update

--- code/DreamJudge1/Source/chapter6/section6_2/1126.cpp
+++ code/DreamJudge1/Source/chapter6/section6_2/1126.cpp
@@ -59,7 +59,7 @@ int main(){
         }
 
         int flag=bfs();
-        if(flag==0) printf("NO\n\n");
+        if(flag==0) printf("No\n\n");
         else{
             for(int i=1;i<=n;i++){
                 for(int j=1;j<=m;j++)
