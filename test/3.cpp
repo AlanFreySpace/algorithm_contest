4cddf24fc5e8d32bfd4a54a6e197d5335b480d3f
AlanFreySpace <976167955@qq.com>
2022-03-10 07:39:34 +0800

    update

--- test/3.cpp
+++ test/3.cpp
@@ -1,8 +1,16 @@
-#include <stdio.h>
-#include <string.h>
-#include <algorithm>
+#include <iostream>
 using namespace std;
 
+#pragma pack(2)
+
+struct how{
+    uint8_t a;
+    uint16_t b;
+    uint8_t c;
+};
+
 int main(){
+    cout<<sizeof(how)<<endl;
     return 0;
 }
+
