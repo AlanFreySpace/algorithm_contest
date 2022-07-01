4a6f7b7bcb7fd69709702116e2b2d05fd4885e5c
AlanFreySpace <976167955@qq.com>
2022-07-01 10:37:06 +0800

    update

---
+++ leetcode/week7/111.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(!root) return 0;
+        if(!root->left) return minDepth(root->right)+1;//无左子树
+        if(!root->right) return minDepth(root->left)+1;//无右子树
+        return min(minDepth(root->left),minDepth(root->right))+1;
+    }
+};
