c2f200c778c070ee4756e624a46d99df22994f3c
AlanFreySpace <976167955@qq.com>
2022-07-01 11:21:29 +0800

    Update 112.cpp

---
+++ leetcode/week7/112.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if(!root) return false;
+        sum-=root->val;
+        if(!root->left&&!root->right) return !sum;//到达叶结点
+        return hasPathSum(root->left,sum)||hasPathSum(root->right,sum);//分别考查左右子树
+    }
+};
