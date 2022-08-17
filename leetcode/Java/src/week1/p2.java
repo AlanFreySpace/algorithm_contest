7fac966c811fc76c423a7a8004672f9697761b1f
AlanFreySpace <976167955@qq.com>
2022-08-17 11:19:30 +0800

    Update p2.java

--- leetcode/Java/src/week1/p2.java
+++ leetcode/Java/src/week1/p2.java
@@ -1,16 +1,21 @@
 package week1;
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-
 public class p2 {
-
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy=new ListNode(-1),cur=dummy;
+        int t=0;
+        while(l1!=null||l2!=null||t!=0){
+            if(l1!=null){
+                t+=l1.val;
+                l1=l1.next;
+            }
+            if(l2!=null){
+                t+=l2.val;
+                l2=l2.next;
+            }
+            cur=cur.next=new ListNode(t%10);
+            t/=10;
+        }
+        return dummy.next;
+    }
 }
