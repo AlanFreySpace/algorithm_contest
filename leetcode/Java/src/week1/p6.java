d1f67b997431cce3dce6508ba72b3c27667fe99d
AlanFreySpace <976167955@qq.com>
2022-08-17 17:30:03 +0800

    Create p6.java

---
+++ leetcode/Java/src/week1/p6.java
@@ -0,0 +1,17 @@
+package week1;
+
+public class p6 {
+	public String convert(String s, int n) {
+		public String convert(String s, int n) {
+	        String res="";
+	        int len=s.length();
+	        for(int i=0;i<n;i++){
+	            if(i==0||i==n-1){
+	                for(int j=i;j<len;j+=(2*n-2)) res+=s.charAt(j);
+	            }else{
+	                
+	            }
+	        }
+	    }
+    }
+}
