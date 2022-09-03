e41b8a67f4cfc292803a4a28bb49709c40e84332
Alan Frey <976167955@qq.com>
2022-09-03 20:06:35 +0800

    update

--- leetcode/Java/src/test/test1.java
+++ leetcode/Java/src/test/test1.java
@@ -2,13 +2,8 @@ package test;
 
 public class test1 {
 	 public static void main(String[] main) {
-		 /*
-		 final long i;
-		 //i=100;
-		 //System.out.println(a.length());
-		 System.out.println(i);
-		 */
-		 int a=5,b=10;
-		 System.out.println((a++)+(++b)+a*b);
+		 int i = 0,j = 0;
+		 boolean booleanValue=(i==j);
+		 System.out.println(booleanValue);
 	 }
 }
