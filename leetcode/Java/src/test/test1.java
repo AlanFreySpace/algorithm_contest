eb678ccb9759cc1fcaf4d6c2236a73abb3a48e6b
Alan Frey <976167955@qq.com>
2022-09-11 21:37:02 +0800

    update

--- leetcode/Java/src/test/test1.java
+++ leetcode/Java/src/test/test1.java
@@ -1,12 +1,23 @@
 package test;
 
 public class test1 {
-	 public static void main(String[] main) {
-		 int x=10,y=30;
-		 do {
-			 y-=x;
-			 x++;
-			 System.out.println(x+" "+y);
-		 }while(x++<y--);
-	 }
+    public static void main(String[] args)  {
+        Outter outter = new Outter();
+        outter.new Inner().print();
+    }
+}
+ 
+ 
+class Outter
+{
+    private int a = 1;
+    class Inner {
+        private int a = 2;
+        public void print() {
+            int a = 3;
+            System.out.println("局部变量：" + a);
+            System.out.println("内部类变量：" + this.a);
+            System.out.println("外部类变量：" + Outter.this.a);
+        }
+    }
 }
