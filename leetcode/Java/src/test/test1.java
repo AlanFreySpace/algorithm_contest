de0974461375b12f05b5b88864ad559df270deac
Alan Frey <976167955@qq.com>
2022-09-12 16:27:40 +0800

    update

--- leetcode/Java/src/test/test1.java
+++ leetcode/Java/src/test/test1.java
@@ -1,23 +1,15 @@
 package test;
 
 public class test1 {
+	int b;
+	
+	test1(int a){
+		b=a;
+	}
+	
+	
     public static void main(String[] args)  {
-        Outter outter = new Outter();
-        outter.new Inner().print();
-    }
-}
- 
- 
-class Outter
-{
-    private int a = 1;
-    class Inner {
-        private int a = 2;
-        public void print() {
-            int a = 3;
-            System.out.println("局部变量：" + a);
-            System.out.println("内部类变量：" + this.a);
-            System.out.println("外部类变量：" + Outter.this.a);
-        }
+        test1 t1=new test1();
+        System.out.println(t1.b);
     }
 }
