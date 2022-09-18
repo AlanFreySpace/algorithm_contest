a7c435963524ef67d0de3c33d8272c3c680c5f67
Alan Frey <976167955@qq.com>
2022-09-18 23:27:41 +0800

    update

--- leetcode/Java/src/test/Test1.java
+++ leetcode/Java/src/test/Test1.java
@@ -1,31 +1,13 @@
 package test;
 	
 public class Test1 {
-	public int test(){
+	public static void main(String[] args){
+		Test1 o = new Test1();
+        System.out.println(o.test());
+    }
+	
+	private int test(){
         System.out.println("test1");
         return 1;
-    }
- 
-    public void test(int a){
-        System.out.println("test2");
-    }   
- 
-    //以下两个参数类型顺序不同
-    public String test(int a,String s){
-        System.out.println("test3");
-        return "returntest3";
-    }   
- 
-    public String test(String s,int a){
-        System.out.println("test4");
-        return "returntest4";
-    }   
- 
-    public static void main(String[] args){
-        Test1 o = new Test1();
-        System.out.println(o.test());
-        o.test(1);
-        System.out.println(o.test(1,"test3"));
-        System.out.println(o.test("test4",1));
     }
 }
