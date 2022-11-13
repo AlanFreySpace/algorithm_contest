7ad0b3cf77463edae923e8a6a85ed9548cd10782
Alan Frey <976167955@qq.com>
2022-11-13 22:26:26 +0800

    Update p51.java

--- leetcode/Java/src/week4/p51.java
+++ leetcode/Java/src/week4/p51.java
@@ -1,5 +1,43 @@
 package week4;
 
-public class p51 {
+import java.util.ArrayList;
+import java.util.List;
 
+public class p51 {
+	List<List<String>> ans=new ArrayList<>();
+    char[][] path;
+    boolean[] usex,usey,usexy,useyx;
+
+    public List<List<String>> solveNQueens(int n) {
+        usex=new boolean[n];
+        usey=new boolean[n];
+        usexy=new boolean[2*n-1];
+        useyx=new boolean[2*n-1];
+        path=new char[n][n];
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++) path[i][j]='.';
+        dfs(n,0);
+        return ans;
+    }
+
+    void dfs(int n,int y){
+        if(y==n){
+        	//应该注意Java中对字符数组和字符串的操作应该怎样进行
+            List<String> list=new ArrayList<>();
+            for(int i=0;i<n;i++){
+                String tmp=new String(path[i]);
+                list.add(tmp);
+            }
+            ans.add(list);
+        }
+        for(int x=0;x<n;x++){
+            if(!usex[x]&&!usey[y]&&!usexy[y-x+n-1]&&!useyx[x+y]){
+                path[y][x]='Q';
+                usex[x]=usey[y]=usexy[y-x+n-1]=useyx[x+y]=true;
+                dfs(n,y+1);
+                usex[x]=usey[y]=usexy[y-x+n-1]=useyx[x+y]=false;
+                path[y][x]='.';
+            }
+        }
+    }
 }
