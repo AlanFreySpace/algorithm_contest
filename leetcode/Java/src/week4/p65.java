6e751b5ed395662242702c1465f2d94169f5a7d8
AlanFreySpace <youwd@chinatelecom.cn>
2022-12-28 16:47:09 +0800

    Update p65.java

--- leetcode/Java/src/week4/p65.java
+++ leetcode/Java/src/week4/p65.java
@@ -1,45 +1,28 @@
 package week4;
 
 public class p65 {
-	public boolean isNumber(String s) {
-        int index=0,res=0;
-        if(s.charAt(index)=='+'||s.charAt(index)=='-') index++;
-        if(index>=s.length()) return false;
-        if(index<s.length()){
-            res=isInteger(s,index);
-            index+=res;
+	int i=0;
+    public boolean isNumber(String s) {
+        boolean numerical=isInteger(s);
+        if(i<s.length()&&s.charAt(i)=='.'){
+            i++;
+            numerical=isUnsignedInteger(s)||numerical;
         }
-        if(index<s.length()){
-            if(res==0&&s.charAt(index)!='.') return false;
-            if(res==0&&s.charAt(index)=='.'){
-                index++;
-                res=isInteger(s,index);
-                if(res==0) return false;
-                else index+=res;
-            }else{
-                if(s.charAt(index)=='.'){
-                    index++;
-                    if(index<s.length()){
-                        res=isInteger(s,index);
-                        index+=res;
-                    }
-                    
-                }
-                else if(s.charAt(index)=='e'||s.charAt(index)=='E') index++;
-                else return false;
-            }
+        if(i<s.length()&&(s.charAt(i)=='e'||s.charAt(i)=='E')){
+            i++;
+            numerical=numerical&&isInteger(s);
         }
-        
-        return true;
+        return numerical&&(i==s.length());
     }
 
-    int isInteger(String s,int index){
-        int len=0;
-        for(int i=index;i<s.length();i++){
-            char tmp=s.charAt(i);
-            if(tmp>='0'&&tmp<='9') len++;
-            else break;
-        }
-        return len;
+    boolean isInteger(String s){
+        if(i<s.length()&&(s.charAt(i)=='+'||s.charAt(i)=='-')) i++;
+        return isUnsignedInteger(s);
+    }
+
+    boolean isUnsignedInteger(String s){
+        int p=i;
+        while(i<s.length()&&s.charAt(i)>='0'&&s.charAt(i)<='9') i++;
+        return p<i;
     }
 }
