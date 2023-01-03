4056d4ea43207f612081dda0fa094ef87711020c
AlanFreySpace <youwd@chinatelecom.cn>
2023-01-03 09:05:44 +0800

    Update p69.java

--- leetcode/Java/src/week4/p69.java
+++ leetcode/Java/src/week4/p69.java
@@ -5,10 +5,9 @@ public class p69 {
 	 * 需要注意的几点:
 	 * 1.二分法:注意mid=(l+r+1)>>1,不能写成mid=(l+r)>>1,否则会死循环
 	 * 2.由于int除法向下取整的特性,因此:
-	 * 1)若mid*mid<x,有mid<=x/mid;
+	 * 1)若mid*mid<x,因为mid是正整数,因此有mid<=x/mid;
 	 * 2)若mid*mid=x,有mid=x/mid;
-	 * 3)若mid*mid>x,有mid>=x/mid;
-	 * 但此处不用担心1)和3)之间冲突,这是因为用的是if...else...结构
+	 * 3)若mid*mid>x,因为mid是正整数,因此有mid>x/mid;
 	 */
 	public int mySqrt(int x) {
         long l=0,r=x;
