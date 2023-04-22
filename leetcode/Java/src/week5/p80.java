3d3b5b8fe0eefb50194662a9c6e4dffc255594a0
Alan Frey <976167955@qq.com>
2023-04-22 20:12:29 +0800

    Create p80.java

---
+++ leetcode/Java/src/week5/p80.java
@@ -0,0 +1,18 @@
+package week5;
+
+public class p80 {
+	public int removeDuplicates(int[] nums) {
+        int count=1,i=0,j=1;
+        for(i=0,j=1;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                count++;
+            }
+            else{
+                nums[j]=nums[i+1];
+                count=1;
+                j++;
+            }
+        }
+        return j;
+    }
+}
