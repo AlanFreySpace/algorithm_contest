9e1c0443b50fe0c3f2c3fa3897702ded24ddc1e9
Alan Frey <976167955@qq.com>
2023-04-22 22:55:41 +0800

    Update p80.java

--- leetcode/Java/src/week5/p80.java
+++ leetcode/Java/src/week5/p80.java
@@ -2,16 +2,16 @@ package week5;
 
 public class p80 {
 	public int removeDuplicates(int[] nums) {
-        int count=1,i=0,j=1;
-        for(i=0,j=1;i<nums.length-1;i++){
-            if(nums[i]==nums[i+1]){
-                count++;
-                if(count<=2) j++;
+        int count=0,i=1,j=1;
+        for(i=1,j=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]){
+                count++; 
             }
             else{
-                nums[j]=nums[i+1];
-                count=1;
-                j++;
+                count=0;
+            }
+            if(count<2){
+                nums[j++]=nums[i];
             }
         }
         return j;
