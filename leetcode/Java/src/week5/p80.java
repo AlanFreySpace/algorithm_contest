06eba91a272950db38d554ca4bb9ddf3a864e305
AlanFreySpace <youwd@chinatelecom.cn>
2023-05-05 16:41:24 +0800

    Update p80.java

--- leetcode/Java/src/week5/p80.java
+++ leetcode/Java/src/week5/p80.java
@@ -2,17 +2,11 @@ package week5;
 
 public class p80 {
 	public int removeDuplicates(int[] nums) {
-        int count=0,i=1,j=1;
-        for(i=1,j=1;i<nums.length;i++){
-            if(nums[i]==nums[i-1]){
-                count++; 
-            }
-            else{
-                count=0;
-            }
-            if(count<2){
-                nums[j++]=nums[i];
-            }
+        int len=nums.length,j=2;
+        if(len<3) return len;
+        for(int i=2;i<len;i++){
+        	// 注意考虑的逻辑:考虑nums[0..j-1](已满足题意的序列)如何扩张
+            if(nums[j-1]!=nums[i]||nums[j-2]!=nums[i]) nums[j++]=nums[i];
         }
         return j;
     }
