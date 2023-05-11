55e87da09210177e118967467740af8cba97a4f9
Alan Frey <976167955@qq.com>
2023-05-11 23:30:51 +0800

    update

--- leetcode/Java/src/week1/ListNode.java
+++ leetcode/Java/src/week1/ListNode.java
@@ -4,6 +4,6 @@ public class ListNode {
     public int val;
     public ListNode next;
     ListNode() {}
-    ListNode(int val) { this.val = val; }
+    public ListNode(int val) { this.val = val; }
     public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
--- leetcode/Java/src/week5/p86.java
+++ leetcode/Java/src/week5/p86.java
@@ -3,6 +3,7 @@ package week5;
 import week1.ListNode;
 
 public class p86 {
+	/**
 	public ListNode partition(ListNode head, int x) {
 	       ListNode small_dummy=new ListNode(-1),big_dummy=new ListNode(-1);
 	       ListNode small_cur=small_dummy,big_cur=big_dummy,cur=head;
@@ -16,6 +17,33 @@ public class p86 {
 	               big_cur=cur;
 	           }
 	           cur=cur.next;
+	       }
+
+	       if(small_dummy.next!=null){
+	           small_cur.next=big_dummy.next;
+	           return small_dummy.next;
+	       }
+
+	       return big_dummy.next;
+	    }
+	    */
+	
+	public ListNode partition(ListNode head, int x) {
+	       ListNode small_dummy=new ListNode(-1),big_dummy=new ListNode(-1);
+	       ListNode small_cur=small_dummy,big_cur=big_dummy,cur=head;
+
+	       while(cur!=null){
+	           if(cur.val<x){
+	               small_cur.next=cur;
+	               small_cur=cur;
+	               cur=cur.next;
+	               small_cur.next=null;
+	           }else{
+	               big_cur.next=cur;
+	               big_cur=cur;
+	               cur=cur.next;
+	               big_cur.next=null;
+	           }
 	       }
 
 	       if(small_dummy.next!=null){
