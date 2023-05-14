2023569ec09c9bd2d2d15fdada79785c18aaf8f5
Alan Frey <976167955@qq.com>
2023-05-14 20:50:18 +0800

    Update p90.java

--- leetcode/Java/src/week5/p90.java
+++ leetcode/Java/src/week5/p90.java
@@ -8,17 +8,18 @@ public class p90 {
     List<List<Integer>> ans=new ArrayList<>();
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
         dfs(nums,0,nums.length);
         return ans;
     }
 
     void dfs(int[] nums,int from,int n){
         ans.add(new ArrayList(path));
-        //if(from==n) return;
 
         for(int i=from;i<n;i++){
             path.add(nums[i]);
             dfs(nums,i+1,n);
+            while(i<n-1&&nums[i]==nums[i+1]) i++;
             path.remove(path.size()-1);
         }
     }
