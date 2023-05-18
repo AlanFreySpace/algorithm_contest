4287f75e7f28d18a795156bcec730196a2a9999c
AlanFreySpace <youwd@chinatelecom.cn>
2023-05-18 14:17:30 +0800

    Update p92.java

--- leetcode/Java/src/week6/p92.java
+++ leetcode/Java/src/week6/p92.java
@@ -4,17 +4,32 @@ import week1.ListNode;
 
 public class p92 {
 	public ListNode reverseBetween(ListNode head, int left, int right) {
-        ListNode dummy=new ListNode(-1,head),before=null,after=null,cur=dummy;
+        ListNode dummy=new ListNode(-1,head),cur=dummy;
+        ListNode before_left=null,left_node=null;
         int i=1;
+
         while(i<left){
             cur=cur.next;
             i++;
         }
-        before=cur;
-        ListNode next1=cur;
-        while(i<right){
-            cur=next1;
-            next1=cur.next;
+        before_left=cur;
+        cur=cur.next;
+        left_node=cur;
+        i++;//此时i==left+1
+
+        ListNode before_cur=cur,after_cur=null;
+        cur=cur.next;
+        while(i<=right){
+            after_cur=cur.next;
+            cur.next=before_cur;
+            before_cur=cur;
+            cur=after_cur;
+            i++;
         }
+
+        before_left.next=before_cur;
+        left_node.next=cur;
+
+        return dummy.next;
     }
 }
