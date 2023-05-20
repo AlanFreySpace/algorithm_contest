b7bf452b9d1a8ce68f4ca47fa4151ac6ef160dc2
Alan Frey <976167955@qq.com>
2023-05-20 16:14:44 +0800

    Update p94_3.java

--- leetcode/Java/src/week6/p94_3.java
+++ leetcode/Java/src/week6/p94_3.java
@@ -9,15 +9,19 @@ public class p94_3 {
         List<Integer> ans=new ArrayList<>();
         Stack<TreeNode> sk=new Stack<>();
         
+        /**
+         * root:当前遍历子树的根节点
+         * sk:用于保存还未进行取值的节点     
+         */
         while(root!=null||!sk.empty()){
-            while(root!=null){
+            while(root!=null){//1.不断向左压入子树根节点
                 sk.push(root);
                 root=root.left;
             }
             root=sk.peek();
             sk.pop();
-            ans.add(root.val);
-            root=root.right;
+            ans.add(root.val);//2.得到中间节点的值
+            root=root.right;//3.转向中间节点的右侧
         }
 
         return ans;
