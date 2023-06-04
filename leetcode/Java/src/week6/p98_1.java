0b73e6994cd206523d5b22cb4f4d1a3bcd1c3767
Alan Frey <976167955@qq.com>
2023-06-04 17:31:20 +0800

    Update p98_1.java

--- leetcode/Java/src/week6/p98_1.java
+++ leetcode/Java/src/week6/p98_1.java
@@ -1,9 +1,16 @@
 package week6;
 
-public class p98 {
+public class p98_1 {
 	int before;
     boolean first=true,ans=true;
 
+    /**
+     * 方法一:
+     * 利用二叉搜索树的性质:
+     * 若二叉树是二叉搜索树，则其中序遍历是升序序列
+     * 代码里体现的是逆否命题：
+     * 若二叉树的中序遍历不是升序序列，则该二叉树不是二叉搜索树
+     */
     public boolean isValidBST(TreeNode root) {
         inOrder(root);
         return ans;
