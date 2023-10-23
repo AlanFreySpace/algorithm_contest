168f0c6250da473646b576b45861243250d28443
Alan Frey <976167955@qq.com>
2023-10-23 23:16:12 +0800

    update

--- leetcode/C++/week7/127.cpp
+++ leetcode/C++/week7/127.cpp
@@ -1,19 +1,19 @@
-class Solution {
+﻿class Solution {
 public:
     unordered_set<string> set;
     unordered_map<string, int> dist;
     queue<string> q;
-
+    //本题是126题的简化版
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         for (auto word : wordList) {
-            set.push(word);
+            set.insert(word);
         }
 
         if (!set.count(endWord)) {
             return 0;
         }
 
-        dist[endWord] = 0;
+        dist[beginWord] = 0;
         q.push(beginWord);
         bool endFlag = false;
 
@@ -22,15 +22,25 @@ public:
             q.pop();
 
             string t = s;
-            for (int i = 0; i < t.size(); t++) {
+            for (int i = 0; i < t.size() && !endFlag; i++) {
                 t = s;
                 for (char j = 'a'; j <= 'z'; j++) {
                     t[i] = j;
                     if (set.count(t) && !dist.count(t)) {
-
+                        dist[t] = dist[s] + 1;
+                        q.push(t);
+                        if (t == endWord) {
+                            endFlag = true;
+                            break;
+                        }
                     }
                 }
             }
         }
+
+        if (!dist.count(endWord)) {
+            return 0;
+        }
+        return dist[endWord] + 1;
     }
 };
