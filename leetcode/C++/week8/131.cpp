910d6a45c5e57505bc0b567682d9b4ce2a1fe7b3
AlanFreySpace <youwd@chinatelecom.cn>
2023-11-10 10:30:08 +0800

    update

--- leetcode/C++/week8/131.cpp
+++ leetcode/C++/week8/131.cpp
@@ -3,25 +3,39 @@ public:
     vector<vector<bool>> f;
     vector<vector<string>> ans;
     vector<string> path;
+    int n;
 
     vector<vector<string>> partition(string s) {
-        int n = s.length();
+        n = s.length();
         f = vector(n, vector<bool>(n));
         for (int j = 0; j < n; j++) {
             for (int i = 0; i <= j; i++) {
                 if (i == j) {
                     f[i][j] = true;
-                    cout << i << " " << j << endl;
                 }
                 else if (s[i] == s[j]) {
                     if (i + 1 > j - 1 || f[i + 1][j - 1]) {
                         f[i][j] = true;
-                        cout << i << " " << j << endl;
                     }
                 }
             }
         }
 
+        dfs(0, s);
         return ans;
+    }
+
+    void dfs(int from, string& s) {
+        if (from == s.size()) {
+            ans.push_back(path);
+            return;
+        }
+        for (int i = from; i < s.size(); i++) {
+            if (f[from][i]) {
+                path.push_back(s.substr(from, i - from + 1));
+                dfs(i + 1, s);
+                path.pop_back();
+            }
+        }
     }
 };
