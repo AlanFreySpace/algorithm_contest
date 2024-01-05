65ccbdb446284e96375d1d630bc8321b7cd2b276
AlanFreySpace <youwd@chinatelecom.cn>
2024-01-05 09:19:32 +0800

    update

--- leetcode/C++/week8/132_2.cpp
+++ leetcode/C++/week8/132_2.cpp
@@ -4,14 +4,23 @@ public:
         int n = s.size();
         vector<int> f(n + 1, 0);
 
+        // 初始化:长度为i的字符串，最多分割i-1次
         for (int i = 0; i <= n; i++) {
             f[i] = i - 1;
         }
 
         for (int i = 1; i <= n; i++) {
+            // 长度为奇数的回文串
             for (int j = 0; i - j >= 1 && i + j <= n && s[i - j - 1] == s[i + j - 1]; j++) {
                 f[i + j] = min(f[i + j], f[i - j - 1] + 1);
             }
+
+            // 长度为偶数的回文串
+            for (int j = 1; i - j >= 0 && i + j <= n && s[i - j] == s[i + j - 1]; j++) {
+                f[i + j] = min(f[i + j], f[i - j] + 1);
+            }
         }
+
+        return f[n];
     }
 };
