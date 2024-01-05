7aeeea5d89e4f43a255e6eb0b3a2187b65ff3e70
AlanFreySpace <youwd@chinatelecom.cn>
2024-01-05 10:46:41 +0800

    update

---
+++ leetcode/C++/week8/133.cpp
@@ -0,0 +1,51 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    unordered_map<Node*, Node*> hash;
+
+    Node* cloneGraph(Node* node) {
+        if (node == NULL) {
+            return NULL;
+        }
+
+        dfs(node);// ¸´ÖÆµã
+
+        for (auto [node, newNode] : hash) { //¸´ÖÆÁÚ±ß
+            for (Node* neighbor : node->neighbors) {
+                newNode->neighbors.push_back(hash[neighbor]);
+            }
+        }
+
+        return hash[node];
+    }
+
+    void dfs(Node* node) {
+        hash[node] = new Node(node->val);
+
+        for (Node* neighbor : node->neighbors) {
+            if (hash.count(neighbor) == 0) {
+                dfs(neighbor);
+            }
+        }
+    }
+};
