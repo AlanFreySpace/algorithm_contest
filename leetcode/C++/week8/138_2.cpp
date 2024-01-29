d5850a2289c938cd409955dea7bbb25b94f55dce
AlanFreySpace <youwd@chinatelecom.cn>
2024-01-29 19:38:35 +0800

    update

--- leetcode/C++/week8/138_2.cpp
+++ leetcode/C++/week8/138_2.cpp
@@ -17,20 +17,20 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        for (auto p = head; p; p = p->next->next) {
+        for (auto p = head; p; p = p->next->next) {// 复制一个小弟
             auto q = new Node(p->val);
             q->next = p->next;
             p->next = q;
         }
 
-        for (auto p = head; p; p = p->next->next) {
+        for (auto p = head; p; p = p->next->next) {// 复制random指针
             if (p->random) {
                 p->next->random = p->random->next;
             }
         }
 
         auto dummy = new Node(-1), cur = dummy;
-        for (auto p = head; p; p = p->next) {
+        for (auto p = head; p; p = p->next) {// 拆分两个链表
             auto q = p->next;
             p->next = q->next;
             cur->next = q;
