94b6285a85c87a009f731563ed1b7cd0c2ccb28d
AlanFreySpace <youwd@chinatelecom.cn>
2024-02-28 10:42:18 +0800

    update

---
+++ leetcode/C++/week8/141.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        if (head == NULL) {
+            return false;
+        }
+
+        ListNode* fast = head, * slow = head;// 快慢指针
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
