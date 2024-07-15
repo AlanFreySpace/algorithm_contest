d5e46295d296027f9e4455172023e928ff333a86
AlanFreySpace <youwd@chinatelecom.cn>
2024-07-15 14:47:50 +0800

    Update 94_3.cpp

--- leetcode/微软/C++/树/94_3.cpp
+++ leetcode/微软/C++/树/94_3.cpp
@@ -15,21 +15,24 @@ public:
         vector<int> ans;
 
         while (root) {
+            // 如果没有左孩子，则直接访问右孩子
             if (!root->left) {
                 ans.push_back(root->val);
                 root = root->right;
             }
             else {
+                // pre节点就是当前root节点向左走一步，然后一直向右走至无法走为止
                 auto pre = root->left;
                 while (pre->right && pre->right != root) {
                     pre = pre->right;
                 }
+                // 说明左子树已经访问完了，我们需要断开链接
                 if (pre->right) {
                     ans.push_back(root->val);
                     root = root->right;
                     pre->right = NULL;
                 }
-                else {
+                else { // 让pre的右指针指向root，继续遍历左子树
                     pre->right = root;
                     root = root->left;
                 }
