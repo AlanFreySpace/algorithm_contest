07997ac0b1d316bad707ff6a67a56d7287e92641
AlanFreySpace <youwd@chinatelecom.cn>
2024-07-15 14:43:17 +0800

    update

--- leetcode/微软/C++/树/94_2.cpp
+++ leetcode/微软/C++/树/94_2.cpp
@@ -9,17 +9,19 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// 方法二：利用栈进行迭代
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         stack<TreeNode*> sk;
         vector<int> ans;
 
-        while (root || !sk.empty()) {
-            while (root) {
+        while (root || !sk.empty()) { // stack为空且root为null时，说明已经遍历结束
+            while (root) { // 可以深入左孩子
                 sk.push(root);
                 root = root->left;
             }
+            // 否则访问栈中节点，并深入右孩子
             root = sk.top();
             sk.pop();
             ans.push_back(root->val);
